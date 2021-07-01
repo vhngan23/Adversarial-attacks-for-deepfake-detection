45735db6c7f1e86e7ce89119051d45cc89232144
khoa <pdkhoa1337@gmail.com>
2021-07-01 16:45:54 +0700

    feat: add datasets

--- dataset.py
+++ dataset.py
@@ -4,15 +4,14 @@ from os.path import isfile, join
 from torch.utils.data import Dataset
 from skimage import io 
 import torch
+from torchvision import datasets
 
 class SingleDirDataset(Dataset):
-    def __init__(self, root_dir,label, transform = None) :
+    def __init__(self, root_dir, transform = None) :
         super().__init__()
         self.root_dir = root_dir
         self.images_paths = [f for f in listdir(root_dir) if isfile(join(root_dir, f))]
         self.transform = transform 
-        #0 for fake 1 for real 
-        self.label = torch.tensor(int(label)) 
     
     def __len__(self):
         return len(self.images_paths)
@@ -20,9 +19,11 @@ class SingleDirDataset(Dataset):
     def __getitem__(self, index):
         img_path = join(self.root_dir, self.images_paths[index])
         image = io.imread(img_path)
-        print(img_path)
+
         if self.transform:
             image = self.transform(image)
         
-        return [image,self.label]
+        return image
 
+def fakeNrealDataset(path, transform = None ):
+    return datasets.ImageFolder(path, transform)
